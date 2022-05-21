93ef96643083452eec1be8130bf6d44e291df9a8
Ronak Harkhani <ronak.harkhani@phonepe.com>
2022-05-21 16:14:50 +0530

    Initial Commit

---
+++ PomodoroTimer/PomodoroTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroTimerApp.swift
+//  PomodoroTimer
+//
+//  Created by Ronak Harkhani on 21/05/22.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
